6ffb48e09c4c7520c935c2e52455427a7341dfbc
Aditya Nath <84228628+MrEvilGamer@users.noreply.github.com>
2022-04-22 14:01:33 +0530

    Update server.lua
    update

--- server.lua
+++ server.lua
@@ -1,30 +1,28 @@
+QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterServerEvent('qb-pizzajob:Payment')
-AddEventHandler('qb-pizzajob:Payment', function()
-	local _source = source
-	local Player = QBCore.Functions.GetPlayer(_source)
+RegisterServerEvent('qb-pizzajob:Payment', function()
+	local src = source
+	local Player = QBCore.Functions.GetPlayer(src)
     Player.Functions.AddMoney("cash", 150, "sold-pizza")
-    TriggerClientEvent("QBCore:Notify", _source, "You recieved $150", "success")
+    TriggerClientEvent("QBCore:Notify", src, "You recieved $150", "success")
 end)
 
-RegisterServerEvent('qb-pizzajob:TakeDeposit')
-AddEventHandler('qb-pizzajob:TakeDeposit', function()
-	local _source = source
-	local Player = QBCore.Functions.GetPlayer(_source)
-    Player.Functions.RemoveMoney("bank", 100, _source, "pizza-deposit")
-    TriggerClientEvent("QBCore:Notify", _source, "You were charged a deposit of $100", "error")
+RegisterServerEvent('qb-pizzajob:TakeDeposit', function()
+	local src = source
+	local Player = QBCore.Functions.GetPlayer(src)
+    Player.Functions.RemoveMoney("bank", 100, src, "pizza-deposit")
+    TriggerClientEvent("QBCore:Notify", src, "You were charged a deposit of $100", "error")
 end)
 
-RegisterServerEvent('qb-pizzajob:ReturnDeposit')
-AddEventHandler('qb-pizzajob:ReturnDeposit', function(info)
-	local _source = source
-    local Player = QBCore.Functions.GetPlayer(_source)
+RegisterServerEvent('qb-pizzajob:ReturnDeposit', function(info)
+	local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
     
     if info == 'cancel' then
         Player.Functions.AddMoney("cash", 50, "pizza-return-vehicle")
-        TriggerClientEvent("QBCore:Notify", _source, "You returned the vehicle and recieved your deposit back", "success")
+        TriggerClientEvent("QBCore:Notify", src, "You returned the vehicle and recieved your deposit back", "success")
     elseif info == 'end' then
         Player.Functions.AddMoney("cash", 150, "pizza-return-vehicle")
-        TriggerClientEvent("QBCore:Notify", _source, "You returned the vehicle and recieved your deposit back", "success")
+        TriggerClientEvent("QBCore:Notify", src, "You returned the vehicle and recieved your deposit back", "success")
     end
 end)
